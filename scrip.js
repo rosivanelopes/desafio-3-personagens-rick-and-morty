d3789d9c6a09d587feb6b6f9927e7f625769a014
Rosivane.Lopes <roselopes.net@gmail.com>
2022-01-16 23:53:08 -0300

    Criar consumo de API Rick and Moorty

--- scrip.js
+++ scrip.js
@@ -3,3 +3,18 @@ const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#status');
+
+
+pegarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.imagem;
+    });
+}
+
+botao.onclick = pegarPersonagem;
